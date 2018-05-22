a8c3709d265b335fa1d49253adc4f1fd3dbde95d
yufang <2435731537@qq.com>
2018-05-22 18:19:16 +0800

    去掉注释部分内容。

--- complete_project/assets/scripts/Star.js
+++ complete_project/assets/scripts/Star.js
@@ -4,11 +4,6 @@ cc.Class({
     properties: {
         // 星星和主角之间的距离小于这个数值时，就会完成收集
         pickRadius: 0,
-        // 暂存 Game 对象的引用
-      // game: {
-        //    default: null,
-        //    serializable: false
-       // }
     },
 
     getPlayerDistance: function () {
